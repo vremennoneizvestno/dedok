170cb3bfc7a6dce8a92d27308e64c7fa3f75554f
dilnaz <sirazhatdenovna@gmail.com>
2023-06-08 13:36:51 +0500

    реализована функция isNotEqual

--- abc-js/practice/utils/string-utils/compare.js
+++ abc-js/practice/utils/string-utils/compare.js
@@ -1,9 +1,10 @@
+import { len } from './len.js';
 
 /** возвращает булевый ответ равны ли параметры firstText и secondText. */
 export function isEqual(firstText, secondText) {
     if (typeof (firstText) !== 'string' || typeof (secondText) !== 'string') throw Error('argument must be type of string');
-    if (firstText.length !== secondText.length) return false;
-    const cycleLength = firstText.length < secondText.length ? firstText.length : secondText.length;
+    if (len(firstText) !== len (secondText)) return false;
+    const cycleLength = len(firstText) < len(secondText) ? len(firstText) : len(secondText);
         for (let i = 0; i < cycleLength; i += 1){
             if (firstText[i] !== secondText[i]) return false;
         }
@@ -12,22 +13,16 @@ export function isEqual(firstText, secondText) {
 
 /** возвращает true если аргументы не равны, и true в иных случаях. */
 export function isNotEqual(firstText, secondText) {
-    if (typeof (firstText) !== 'string' || typeof (secondText) !== 'string') throw Error('argument must be type of string');
-    if (firstText.length !== secondText.length) return true;
-    const cycleLength = firstText.length < secondText.length ? firstText.length : secondText.length;
-        for (let i = 0; i < cycleLength; i += 1){
-            if (firstText[i] !== secondText[i]) return true;
-        }
-    return false;
+    return !isEqual(firstText,secondText)
 }
 
 /** возвращает булевый ответ больше ли параметр firstText чем secondText. */
 export function isMore(firstText, secondText) {
     if (typeof (firstText) !== 'string' || typeof (secondText) !== 'string') throw Error('argument must be type of string');
-    const cycleLength = firstText.length < secondText.length ? firstText.length : secondText.length;
-        for (let i = 0; i < cycleLength; i += 1){
-            if (firstText[i] > secondText[i]) return true;
-        }
+    if (len(firstText) > len(secondText)) return true;
+    for (let i = 0; i <= len (firstText); i += 1){
+        if (firstText[i] > secondText[i]) return true;
+    }
     return false;
 }
 
