20fcfe3d8b33bc97bfd43d5d60c04a743fa7925b
dilnaz <sirazhatdenovna@gmail.com>
2023-06-08 13:32:38 +0500

    реализована функция len

--- abc-js/practice/utils/string-utils/len.js
+++ abc-js/practice/utils/string-utils/len.js
@@ -1,2 +1,5 @@
 export function len(text) {
+    if (text === '') return 0;
+    if (typeof(text) !== 'string') throw Error ('argument must be type of string')
+    for (var textLength = 0; typeof(text[textLength]) === 'string'; textLength +=1) {}return textLength;
 }
